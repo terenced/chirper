34531c44ffa1702548be843d6ba808bbc1e6c847
terenced <terry.dellino@gmail.com>
2013-10-09 12:29:21 -0400

    follow/unfollow api end points + url clean ups

--- chirper_api/urls.py
+++ chirper_api/urls.py
@@ -4,15 +4,14 @@ import views
 user_urls = patterns('',
     url(r'^$', views.UserList.as_view(), name='user-list'),
     url(r'^/(?P<username>[0-9a-zA-Z_-]+)/$', views.UserDetails.as_view(), name='user-details'),
+    url(r'^/(?P<username>[0-9a-zA-Z_-]+)/follow/(?P<followed>[0-9a-zA-Z_-]+)$', views.follow, name='follow'),
 )
 
 chirps_urls = patterns('',
-    url(r'^$', views.ChirpsList.as_view(), name='chirps-list'),
-    url(r'^/(?P<username>[0-9a-zA-Z_-]+)$', views.ChirpsTimeline.as_view(), name='chirp-timeline'),
-
+    url(r'^/timeline$', views.ChirpsList.as_view(), name='chirp-timeline'),
 )
 
 urlpatterns = patterns('',
     url(r'^users', include(user_urls)),
-    url(r'chirps', include(chirps_urls)),
+    url(r'^chirps', include(chirps_urls)),
 )
--- chirper_api/views.py
+++ chirper_api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.db.models import Q
 
 import serializers
@@ -14,15 +16,40 @@ class UserDetails(generics.RetrieveAPIView):
     serializer_class = serializers.UserSerializer
     lookup_field = 'username'
 
+@api_view(['POST', 'DELETE'])
+def follow(request, username, followed):
+
+    try:
+        user = models.User.objects.get(username = username)
+        followed = models.User.objects.get(username = followed)
+
+    except Exception, e:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'POST':
+        if user.id == followed.id:
+            return Response(status=status.HTTP_304_NOT_MODIFIED)
+
+        user.profile.follows.add(followed.profile)
+        user.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+    if request.method == 'DELETE':
+        user.profile.follows.remove(followed.profile)
+        user.save()
+        return Response(status=status.HTTP_200_OK)
+
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 class ChirpsList(generics.ListAPIView):
     serializer_class = serializers.ChirpSerializer
-    user = models.User.objects.get(id=1)
-    queryset = models.Chirp.objects \
-                           .filter(Q(user=user)| Q(user__userprofile__in=user.profile.follows.all)) \
-                           .order_by('-created_on')
 
+    def get_queryset(self):
+        user = models.User.objects.get(id=1)#self.request.user
+        return models.Chirp.objects \
+                          .filter(Q(user=user)| Q(user__userprofile__in=user.profile.follows.all)) \
+                          .order_by('-created_on')
 
-class ChirpsTimeline(generics.ListAPIView):
-    serializer_class = serializers.ChirpSerializer
-    queryset = models.Chirp.objects.all()
