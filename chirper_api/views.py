7affeb33ceb2b4a944b3fdecd8de111ee24240d0
Terry Dellino <terry.dellino@gmail.com>
2013-10-11 15:54:37 -0400

    new register user view which returns token when user is regd

--- chirper_api/views.py
+++ chirper_api/views.py
@@ -2,11 +2,33 @@ from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
 from django.db.models import Q
 
 
 import serializers
 import models
+
+@api_view(['POST'])
+def register_user(request):
+    user = models.User()
+    try:
+        user.username = request.DATA["username"]
+        user.password = request.DATA["password"]
+
+        if request.DATA.has_key("first_name"):
+            user.first_name = request.DATA["first_name"]
+
+        if request.DATA.has_key("last_name"):
+            request.DATA["last_name"]
+
+        user.save()
+        token = Token.objects.get(user__username = user.username)
+        return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+    except Exception, e:
+        print e
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserRegistration(generics.CreateAPIView):
     permission_classes = (AllowAny,)
