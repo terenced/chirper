dc330a0f720fbc5d737f80226ff0286a1e83c4c9
Terry Dellino <terry.dellino@gmail.com>
2013-10-11 15:55:36 -0400

    populate fist and last name if empty on pre save

--- chirper_api/models.py
+++ chirper_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save, post_save
 from rest_framework.authtoken.models import Token
 
 
@@ -10,6 +10,13 @@ from rest_framework.authtoken.models import Token
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+@receiver(pre_save, sender=User)
+def populate_first_last_name(sender, instance=None, created=False, **kwargs):
+    if instance.first_name == '':
+        instance.first_name = "Anony"
+    if instance.last_name == '':
+        instance.last_name = "Mous"
 
 class Chirp(models.Model):
     content = models.CharField(max_length=140)
